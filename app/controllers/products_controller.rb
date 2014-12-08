d961d0fe43d799a03e191a1cd7ff84a92d718061
postazure <gordonmaxc@gmail.com>
2014-12-08 10:08:42 -0800

    can add products, and view on show page

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,27 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+
+    redirect_to @product
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+
+    end
+end
